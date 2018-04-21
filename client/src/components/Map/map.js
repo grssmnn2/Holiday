cc82879a52b03d79c9ac2fb7c1149c5222f12ee5
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-04-21 12:05:02 -0500

    added pseudocode

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,5 +1,6 @@
 //  external dependencies 
 import GoogleMapReact from 'google-map-react'
+
 //  code dependencies 
 
 //  map goes here
@@ -13,6 +14,10 @@ export default Map;
 //  ok so the coordinates are going to be props that are passed down from the result component 
 //  and they would match the location / details that are provided in the card results 
 //  I'll have to check the schema for that, too see where we are storing location data 
+//  map result would be called in render array.map method when the results are being rendered to the screen 
 //  and how we are storing that
-//  for now, can provide coordinates of Hawaii 
+//  for now, can provide coordinates of Hawaii  props.lat props.lgn => where would I be finding them>
+//  initially thinking it's prolly from address 
+//  Js library that converts address into lat/lgn or maybe there is something easier there 
+
 
