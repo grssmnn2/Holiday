ffe9e87a44740f20a14e1554cfe942a6e65d378f
eddiezhaor <eddiezhaor@gmail.com>
2018-04-21 10:52:21 -0500

    nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import Chatbox from "./components/Chatbox"
 import Imageuploader from "./components/Imageuploder"
+import Navbar from "./components/Navbar"
 import "./App.css";
-
+import "./css/bootstrap.css"
+import "./css/main.css"
 class App extends Component {
   render() {
     return (
-      <div>
-      <Chatbox></Chatbox>
-      <Imageuploader></Imageuploader>
-      </div>
+     <body>
+      {/* <Chatbox></Chatbox>
+      <Imageuploader></Imageuploader> */}
+      </body>
     );
   }
 }
