551bf2bd80da6803a099c7e4d0329273a925a6be
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-04-20 21:56:00 -0500

    removed extra components

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,15 @@ import Chatbox from "./components/Chatbox"
 // import Result from "./components/pages/Result"
 //  other static components go here too
 import "./App.css";
-import Header from "./components/Header"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+
 
 
 class App extends Component {
   render() {
     return (
       <div>
-
-        <Header />
-        <Navbar />
       <Chatbox></Chatbox>
-      <Footer />
+
 
       </div>
     );
