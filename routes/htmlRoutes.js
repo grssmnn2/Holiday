062a08307c6256d981d7fe3c5c6616710e09f686
Cecily Grossmann <grssmnn2@gmail.com>
2018-04-20 23:15:11 -0500

    upload button style

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,20 @@
 var router = require("express").Router();
+
+(() => {
+
+    const path = require("path");
+    const db = require("../models");
+  
+    const isAuthenticated = require("../config/middleware/isAuthenticated");
+    
+  
+    module.exports = app => {
+  
+      // homepage - options to login or signup
+      app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/template/realestate/index.html"));
+      });  
+  
+    };
+  
+  })();
