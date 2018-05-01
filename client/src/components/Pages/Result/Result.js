a19e09a4c718978f11f399567e7e6630b0720ede
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-01 04:55:16 -0500

    API requests

--- client/src/components/Pages/Result/Result.js
+++ client/src/components/Pages/Result/Result.js
@@ -1,84 +1,81 @@
-import React, {Component} from "react";
+import React, { Component } from "react"
 
-//  other dependencies go here -both dynamic and static 
-import React, { Component} from "react";
-import { Col, Row, Container} from "../components/Grid";
-import API from "../../utils/API";
-//  figure out exactly 
-import Card from "../components/Card";
-//  import google maps  component here 
-
- 
+//  other dependencies go here -both dynamic and static
+import React, { Component } from "react"
+import { Col, Row, Container } from "../components/Grid"
+import API from "../../utils/API"
+//  figure out exactly
+import Card from "../components/Card"
+//  import google maps  component here
 
 class Result extends Component {
-    state = { 
-        address: "",
-        city: "",
-        state: "",
-        country: "",
-        zip: "",
-        pets: "",
-        bathroom: "",
-        bedroom: "",
-        guests: "",
-        wifi: "",
-        review: ""
-    }
+  state = {
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    zip: "",
+    pets: "",
+    bathroom: "",
+    bedroom: "",
+    guests: "",
+    wifi: "",
+    review: ""
+  }
 
-    componentWillMount() {
-        this.displayResults();
-    }
+  componentWillMount() {
+    this.displayResults()
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+      this.saveResults()
+  }
 
-    }
+  loadResults = () => {
+    this.setState({
+      address: listing.address,
+      city: listing.city,
+      state: listing.state,
+      country: listing.country,
+      zip: listing.zip,
+      pets: listing.pets,
+      bathroom: listing.bathroom,
+      bedroom: listing.bedroom,
+      guests: listing.guests,
+      wifi: listing.wifi,
+      review: listing.review
+    })
+  }
+  //    testing with only a few for now but adding more 
 
-    loadResults = () => {
-        this.setState({
-            address: listing.address,
-            city: listing.city,
-            state: listing.state,
-            country: listing.country,
-            zip: listing.zip,
-            pets: listing.pets,
-            bathroom: listing.bathroom,
-            bedroom: listing.bedroom,
-            guests: listing.guests,
-            wifi: listing.wifi,
-            review: listing.review
-        });
-    };
+  displayResults = () => {
+    API.getResults(this.state.adddress, this.state.city, this.state.country)
+      .then(res => {
+        console.log("this.state.address", "this.state.city", "this.state.country")
+      })
+      .catch(err => console.log(err))
+  }
 
-    displayResults = () => {
-       API.getResults()
-       .then(res =>{
-           console.log("data req succeeded")
-       }).catch(err => console.log(err))
+  //  methods for handling clicks/toggles/input changes
+  //  i.e. it'd be nice to enlarge the photo on hover, idk
 
-    }
+  //  start render
+  //  return a div that contains search results on one side and map on the other
 
-//  methods for handling clicks/toggles/input changes 
-//  i.e. it'd be nice to enlarge the photo on hover, idk
-
-//  start render 
-//  components that are required above go here 
-//  return a div that contains search results on one side and map on the other 
-
-render() {
+  render() {
     return (
-        <div>
-            <Card />
-        </div>
-        //  testing this in the app file for now, for the first iteration 
-        //  array function here to cycle thru all elements that were returned 
-        //  map thru array that is equal to matches.length 
+      <div>
+        <Card />
+      </div>
+      //  testing this in the app file for now, for the first iteration
+      //  array function here to cycle thru all elements that were returned
+      //  map thru array that is equal to matches.length
 
-        // <Map /> 
+      // <Map />
 
-        //  then render that map 
-    );
- }
+      //  then render that map
+    )
+  }
+}
 
-};
-
-export default Result; 
+export default Result
