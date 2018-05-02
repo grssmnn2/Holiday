9e333ec36d2dbf5a6b1870717c9ceb43290b4926
Jennifer <white.jenniferann@gmail.com>
2018-05-01 21:22:52 -0500

    load image

--- client/src/components/Pages/Profile/Profile.js
+++ client/src/components/Pages/Profile/Profile.js
@@ -2,29 +2,38 @@ import React, { Component } from "react";
 
 import Navbar from "../../Navbar"
 import Footer from "../../Footer"
+import loadImageURL from './load-image-url'
 
 
 class Profile extends Component {
     render() {
-        return (
-            <div>
-             
-
-            <div className="container pt-30">
-				<div className="row height align-items-center justify-content-center">
-					<div className="col-lg-10">
-						<div className="generic-banner-content">
-								<h2 className="text-white text-center">Welcome back!</h2>
-								<p className="text-white">Manage your saved homes and view your messages here.</p>
-						</div>							
-					</div>
-				</div>
-			</div>
+        return ( <
+            div >
 
 
-       
-            </div >
-       
+            <
+            div className = "container pt-30" >
+            <
+            div className = "row height align-items-center justify-content-center" >
+            <
+            div className = "col-lg-10" >
+            <
+            div className = "generic-banner-content" >
+            <
+            h2 className = "text-white text-center" > Welcome back! < /h2> <
+            p className = "text-white" > Manage your saved homes and view your messages here. < /p>
+
+            <
+            /div>						 <
+            /div> <
+            /div> <
+            /div>
+
+
+
+            <
+            /div >
+
         )
     }
 }
