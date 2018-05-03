83bc460c1b9e9c1361dc2f2ab080e776778fc4f7
Jennifer <white.jenniferann@gmail.com>
2018-05-02 21:02:28 -0500

    image uploader changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export default {
-    storeMessage: (message) => axios.post("/api/message/",message),
-    findhistoryMessage:(sender,receiver) => axios.get("/api/historymessage/"+sender+"/"+receiver),
-    retrieveFriendList:(user) => axios.get("api/friendlist/"+user),
-    addFriends:(name,friend) => axios.post("api/friendlist/"+name,friend),
-    getResults:(city) => axios.get("/api/results/"+city)
+    storeMessage: (message) => axios.post("/api/message/", message),
+    findhistoryMessage: (sender, receiver) => axios.get("/api/historymessage/" + sender + "/" + receiver),
+    retrieveFriendList: (user) => axios.get("api/friendlist/" + user),
+    addFriends: (name, friend) => axios.post("api/friendlist/" + name, friend),
+    getResults: (city) => axios.get("/api/results/" + city),
+    updateList: (images) => axios.post("api/friendlistpost/" + email, images)
+
 }
