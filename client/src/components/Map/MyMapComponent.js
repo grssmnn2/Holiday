02c8e24717150d3fe4b93db954bae894d38ba437
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-03 19:35:12 -0500

    rendering map and results on page

--- client/src/components/Map/MyMapComponent.js
+++ client/src/components/Map/MyMapComponent.js
@@ -24,12 +24,12 @@ let address = "181 Madison St, Sag Harbor, NY";
        }
    );
 
-   <MyMapComponent isMarkerShown
-   googleMapURL="https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places"
-   loadingElement={<div style={{ height: `100%` }} />}
-   containerElement={<div style={{ height: `400px` }} />}
-   mapElement={<div style={{ height: `100%` }} />}
- />
+//    <MyMapComponent isMarkerShown
+//    googleMapURL="https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places"
+//    loadingElement={<div style={{ height: `100%` }} />}
+//    containerElement={<div style={{ height: `400px` }} />}
+//    mapElement={<div style={{ height: `100%` }} />}
+//  />
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) => 
 <GoogleMap 
