fe3506a44db0e8302f2340ecca1ac37dc2c1dbb9
Jennifer <white.jenniferann@gmail.com>
2018-05-05 11:31:50 -0500

    profile js

--- client/src/components/Pages/Profile/Profile.js
+++ client/src/components/Pages/Profile/Profile.js
@@ -3,7 +3,6 @@ import Navbar from "../../Navbar"
 import Footer from "../../Footer"
 import React, { Component } from 'react';
 import { Panel, ControlLabel, Glyphicon } from 'react-bootstrap';
-import './Profile.css';
 
 class Profile extends Component {
     componentWillMount() {
@@ -47,8 +46,6 @@ class Profile extends Component {
     render() {
         return ( <
             div >
-
-
             <
             div className = "container pt-30" >                  < div className = "row height align-items-center justify-content-center" >                      < div className = "col-lg-10" >                          < div className = "generic-banner-content" >                                  < h2 className = "text-white text-center" > Welcome back! < /h2>                                 < p className = "text-white" > Manage your saved homes and view your messages here. < /p>                         < /div>                                               < /div>                 < /div>             < /div>
 
