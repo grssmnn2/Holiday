fe01e6b195d15428e30f9739649fc1f25a8a3835
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-08 19:36:38 -0500

    styling of result page is complete and removed extra CSS file

--- client/src/components/Pages/Result/Result.js
+++ client/src/components/Pages/Result/Result.js
@@ -3,17 +3,10 @@ import Friendlist from "../../Friendlist"
 import Footer from "../../Footer";
 import { Modal, Button } from "antd";
 import API from "../../../utils/API";
-import './Result.css'
+// import './Result.css'
 import MyMapComponent from "../../Map";
 import { Card } from "antd";
 const { Meta } = Card;
-//  we can access the address using this.state.address 
-//  pass this into the geo coding?  - and putting the geo coding
-//  examples of passing state 
-//  so the home page is parent of result page
-//  and whatever result is passed, set state for result
-//  default can be chicago 
-//  this.props.location.state.city
 
 class Result extends Component {
   state = {
@@ -127,7 +120,7 @@ class Result extends Component {
         <div className="main-content" style={{ padding: "5em" }}>
           <div className="workspace">
           <div className="row">
-          <div className="col"> 
+          <div className="col-md-6"> 
           
           {friend.map(result => {
               return (
@@ -155,7 +148,7 @@ class Result extends Component {
               
             })}
           </div>
-          <div className="col">  col 2 
+          <div className="col-md-6">  col 2 
           {this.state.isMap?null:<MyMapComponent isMarkerShown={true}
    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZ0UrBlp4cZvjyvOfJthUB1jPyj1X4pn4&v=3.exp&libraries=geometry,drawing,places"
    loadingElement={<div style={{ height: `100%` }} />}
@@ -163,46 +156,13 @@ class Result extends Component {
    mapElement={<div style={{ height: `100%` }} />}
    data={this.state}
  />} </div>
-
-
-
            </div>
-            {/* {friend.map(result => {
-              return (
-                <Card
-                  hoverable
-                  style={{ width: 150, float: "left", marginBottom: 40, height: 373, marginRight: 30 }}
-                  cover={
-                    <img
-                      alt="example"
-                      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    />
-                  }
-                >
-                  <Meta
-                    title={result.name}
-                    description={
-                      "city:" + result.city 
-                    }
-                  />
-                </Card>
-               
-              );
-              
-            })} */}
-
-            {/* {this.state.isMap?null:<MyMapComponent isMarkerShown={true}
-   googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZ0UrBlp4cZvjyvOfJthUB1jPyj1X4pn4&v=3.exp&libraries=geometry,drawing,places"
-   loadingElement={<div style={{ height: `100%` }} />}
-   containerElement={<div style={{ width:"200px" ,position:"fixed",height: `500px` }} />}
-   mapElement={<div style={{ height: `100%` }} />}
-   data={this.state}
- />} */}
+           
           </div>
         </div>
       </div>
     );
   }
-// }
+
 }
 export default Result;
