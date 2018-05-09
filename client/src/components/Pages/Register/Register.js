04b95d04884a3177a197f6ea902737e2ae157eff
Aditi Mishra <asmishra93@gmail.com>
2018-05-08 21:02:12 -0500

    Autocomplete functional

--- client/src/components/Pages/Register/Register.js
+++ client/src/components/Pages/Register/Register.js
@@ -162,41 +162,56 @@ class Register extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div id="locationField">
-                                <input id="autocomplete" placeholder="Testing autocomplete" onFocus={geolocate()} type="text"></input>
+
+                            {/* <div id="locationField">
+                                <input id="autocomplete" placeholder="Begin Typing Your Address Here" onFocus={geolocate()} type="text"></input>
+                            </div> */}
+                            
+                            <div className="form-group" id = "locationField" >
+                                <label htmlFor="address" className="cols-sm-2 control-label">Address</label>
+                                <div className="cols-sm-10">
+                                    <div className="input-group">
+                                        <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                        <input id="autocomplete" onFocus={geolocate()} type="text" className="form-control" placeholder="Begin Typing Your Address" />
+                                    </div>
+                                </div>
                             </div>
-                            <table id="address">
+
+                            <table>
                                 <tr>
-                                    <td class="label">Street address</td>
-                                    <td class="slimField"><input class="field" id="street_number"
+                                    <td className="label">Street address</td>
+                                    <td className="slimField"><input className="field" id="street_number"
                                     disabled="true"></input></td>
-                                    <td class="wideField" colspan="2"><input class="field" id="route" disabled="true"></input></td>
+                                    <td className="wideField" colspan="2"><input class="field" id="route" disabled="true"></input></td>
                                 </tr>
                                 <tr>
-                                    <td class="label">City</td>
-                                    <td class="wideField" colspan="3"><input class="field" id="locality" disabled="true"></input></td>
+                                    <td className="label">City</td>
+                                    <td className="wideField" colspan="3"><input className="field" id="locality" disabled="true"></input></td>
                                 </tr>
                                 <tr>
-                                    <td class="label">State</td>
-                                    <td class="slimField"><input class="field" id="administrative_area_level_1" disabled="true"></input></td>
-                                    <td class="label">Zip code</td>
-                                    <td class="wideField"><input class="field" id="postal_code"
+                                    <td className="label">State</td>
+                                    <td className="slimField"><input className="field" id="administrative_area_level_1" disabled="true"></input></td>
+                                    <td className="label">Zip code</td>
+                                    <td className="wideField"><input className="field" id="postal_code"
                                     disabled="true"></input></td>
                                 </tr>
                                 <tr>
-                                    <td class="label">Country</td>
-                                    <td class="wideField" colspan="3"><input class="field" id="country" disabled="true"></input></td>
+                                    <td className="label">Country</td>
+                                    <td className="wideField" colspan="3"><input className="field" id="country" disabled="true"></input></td>
                                 </tr>
                             </table>
+                            
                             <div className="form-group">
                                 <label htmlFor="address" className="cols-sm-2 control-label">Address</label>
                                 <div className="cols-sm-10">
                                     <div className="input-group">
                                         <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" onChange={(e)=>this.handleChange(e)} value={this.state.address}name="address" id="confirm" placeholder="Enter Your Address" />
+                                        <input type="text" className="form-control field" id="street_number" onChange={(e)=>this.handleChange(e)} value={this.state.address}name="address" id="confirm" placeholder="Enter Your Address" />
+                                        <input class="field" id="route" disabled="true" />
                                     </div>
                                 </div>
                             </div>
+
                             <div className="form-group">
                                 <label htmlFor="confirm" className="cols-sm-2 control-label">Country</label>
                                 <div className="cols-sm-10">
